805271c20a964be92c07e9d0fc6892bc6208a0e6
psitto <59241379+psitto@users.noreply.github.com>
2023-08-06 20:02:44 -0300

    Add Easing.c to hold easing function definitions

--- src/Easing.h
+++ src/Easing.h
@@ -6,26 +6,9 @@
 
 // thank you https://easings.net/
 
-float ease_linear(float x)
-{
-	return x;
-}
-
-float ease_out_sine(float x)
-{
-	return sin(x * M_PI_2);
-}
-
-float ease_in_sine(float x)
-{
-	return 1 - cos(x * M_PI_2);
-}
-
-float ease_out_back(float x)
-{
-	static const float c1 = 1.70158;
-	static const float c3 = 2.70158;
-	return 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2);
-}
+float ease_linear(float x);
+float ease_out_sine(float x);
+float ease_in_sine(float x);
+float ease_out_back(float x);
 
 #endif
